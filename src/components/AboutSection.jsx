bb276d2d31709033f945e25360ad8f3c3491ebdd
AntoineMHB <antoinemuhumbania@gmail.com>
2025-08-21 14:12:11 +0200

    Finish about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,7 +16,7 @@ export default function AboutSection() {
           </h1>
 
           <div className="mt-20 w-[1030px] pl-[320px]">
-            <h2 className="text-2xl text-justify">
+            <h2 className="text-xl text-justify">
               Our mission is to enliven your road trip with the perfect travel
               van rental. Our vans are recertified before each trip to ensure
               your travel plans can go off without a hitch. (Hitch costs extra
@@ -24,11 +24,22 @@ export default function AboutSection() {
             </h2>
           </div>
 
-          <div className="mt-20 w-[1030px] pl-[320px]">
-            <h2 className="text-2xl text-justify">
+          <div className="mt-10 w-[1030px] pl-[320px]">
+            <h2 className="text-xl text-justify">
               Our team is full of vanlife enthusiasts who know firsthand the
               magic of touring the world on 4 wheels.
             </h2>
+          </div>
+        </div>
+
+        <div className="mt-20 mb-20 ml-[320px] mr-[320px] w-[720px] h-[300px] bg-[#FFCC8D]">
+          <div className="pt-20 ml-10">
+            <h2 className="text-2xl font-bold">Your destination is waiting.</h2>
+            <h2 className="text-2xl font-bold mt-0.5">Your van is ready.</h2>
+
+            <button className="mt-[30px] w-[200px] h-[50px] rounded-[5px] bg-black text-white font-bold">
+              Explore our vans
+            </button>
           </div>
         </div>
       </div>
